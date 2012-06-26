6d3c9e766fb3d540850a8376d1b7774f083766de
Shinichi Tomita <shinichi.tomita@gmail.com>
2012-06-26 23:22:14 +0900

    add configuration file

---
+++ test/config/salesforce.js
@@ -0,0 +1,9 @@
+module.exports = {
+  loginServerUrl : "https://login.salesforce.com",
+  username :       process.env.SF_USERNAME,
+  password :       process.env.SF_PASSWORD,
+  clientId :       process.env.SF_OAUTH2_CLIENT_ID,
+  clientSecret :   process.env.SF_OAUTH2_CLIENT_SECRET,
+  redirectUri :    "http://localhost:4000/oauth2/callback",
+  bigTable :       "BigTable__c"
+};
